fa266aa9b5ff52d4f74e171d2440fe0034d3c922
vaishnavi90175 <2400090175@kluniversity.in>
2026-03-31 08:04:08 +0530

    Delete Course-Management-1 directory

--- Course-Management-1/src/main/java/com/example/course/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.course.repository;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.course.entity.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-    List<Course> findByTitleContainingIgnoreCase(String title);
-}
